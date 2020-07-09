72e20f262ac66ab0310c387d30da0aa121894a01
nemmtor <kacper0witas@gmail.com>
2020-07-09 07:47:24 +0200

    Add line height, fix smooth scroll

--- src/utils/smoothScroll.js
+++ src/utils/smoothScroll.js
@@ -1,12 +1,16 @@
 import jump from 'jump.js';
 
 const dataScrollElements = document.querySelectorAll('[data-scroll]');
+const nav = document.querySelector('.nav');
 
-dataScrollElements.forEach((element) => {
-    const { target } = element.dataset;
-    element.addEventListener('click', () => {
-        jump(target, {
-            offset: -48,
+document.addEventListener('DOMContentLoaded', () => {
+    const navHeight = nav.offsetHeight;
+    dataScrollElements.forEach((element) => {
+        const { target } = element.dataset;
+        element.addEventListener('click', () => {
+            jump(target, {
+                offset: -navHeight,
+            });
         });
     });
 });
