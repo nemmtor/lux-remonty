4c533c6f0a3c31c35eea607719584fb23cd14824
nemmtor <kacper0witas@gmail.com>
2020-07-09 14:21:25 +0200

    Add lodash debounce

--- src/utils/smoothScroll.js
+++ src/utils/smoothScroll.js
@@ -1,18 +1,22 @@
 import jump from 'jump.js';
+import { debounce } from 'lodash';
 
 const dataScrollElements = document.querySelectorAll('[data-scroll]');
 const nav = document.querySelector('.nav');
 
-const laptopView = window.matchMedia('screen and (min-width: 1024px)');
-
 document.addEventListener('DOMContentLoaded', () => {
     const navHeight = nav.offsetHeight;
+
+    const jumpDebounced = debounce((target) => {
+        jump(target, {
+            offset: -navHeight,
+        });
+    }, 500);
+
     dataScrollElements.forEach((element) => {
         const { target } = element.dataset;
         element.addEventListener('click', () => {
-            jump(target, {
-                offset: -navHeight,
-            });
+            jumpDebounced(target);
         });
     });
 });
