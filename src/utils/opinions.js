18d40811e126aaeb50e6d7a4b6b61adace084e8d
nemmtor <kacper0witas@gmail.com>
2020-07-09 15:37:04 +0200

    Working on adding navigation to opinions

--- src/utils/opinions.js
+++ src/utils/opinions.js
@@ -1,6 +1,10 @@
 import gsap from 'gsap';
 
+// const prev = document.querySelector('.opinions__prev');
+// const next = document.querySelector('.opinions__next');
+
 const TIME_BETWEEN_SLIDES = 2;
+// const ROUND_TO = 1 / 3;
 
 const opinionsTimeline = gsap.timeline({
     repeat: -1,
@@ -12,16 +16,30 @@ const opinionsTimeline = gsap.timeline({
     },
 });
 
+// const duration = opinionsTimeline.duration();
+
 opinionsTimeline
-    .to('.opinion', {
-        xPercent: -100,
-    })
-    .to('.opinion', {
-        xPercent: -200,
-    })
-    .to('.opinion', {
-        xPercent: -300,
-    })
+    .to(
+        '.opinion',
+        {
+            xPercent: -100,
+        },
+        'opinion1',
+    )
+    .to(
+        '.opinion',
+        {
+            xPercent: -200,
+        },
+        'opinion2',
+    )
+    .to(
+        '.opinion',
+        {
+            xPercent: -300,
+        },
+        'opinion3',
+    )
     .set('.opinion', {
         x: 0,
         delay: 0,
@@ -44,3 +62,23 @@ scrollTopTimeline
     .set('.footer__scroll-img', {
         yPercent: 100,
     });
+
+// prev.addEventListener('click', () => {
+//     console.log(opinionsTimeline.currentLabel());
+//     // const time =
+//     //     duration *
+//     //     Math.round(opinionsTimeline.progress() / ROUND_TO) *
+//     //     ROUND_TO;
+//     // console.log(time);
+//     // opinionsTimeline.tweenTo(time);
+//     // opinionsTimeline.resume();
+// });
+
+// next.addEventListener('click', () => {
+//     const time =
+//         duration *
+//         Math.round(opinionsTimeline.progress() / ROUND_TO) *
+//         ROUND_TO;
+//     opinionsTimeline.tweenTo(time);
+//     opinionsTimeline.resume();
+// });
